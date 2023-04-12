fd77a3cd7b5a680676fc6b31578d3ec3f562059a
fistaco <fiske_schijlen@hotmail.com>
2023-04-12 20:39:08 +0200

    Implement an API endpoint allowing users to run a parametrised binary GA.

--- src/main/java/com/example/demo/GeneticAlgorithmController.java
+++ src/main/java/com/example/demo/GeneticAlgorithmController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.entity.GeneticAlgorithm;
 import com.example.entity.CrossoverType;
+import com.example.entity.GeneticAlgorithm;
 import com.example.entity.GenomeType;
 
 
@@ -15,4 +17,22 @@ import com.example.entity.GenomeType;
 @RestController
 public class GeneticAlgorithmController {
     
+    /**
+     * Runs a genetic algorithm with the provided parameters, stores the results, and returns the
+     * best resulting genome to the user.
+     * @return a string representation of the best genome upon termination of the GA.
+     * @throws Exception
+     */
+    @GetMapping("/binaryga")
+    public String runBinaryGeneticAlgorithm(
+        @RequestParam int popSize,
+        @RequestParam int gens,
+        @RequestParam int binaryStringLength) throws Exception {
+        List<Object> genomeInitialisationArgs = List.of(binaryStringLength);
+        var binaryGa = new GeneticAlgorithm(popSize, gens, CrossoverType.ONEPOINT, 0.5, 0.5, binaryStringLength, GenomeType.BINARYSTRING);
+
+        binaryGa.run(genomeInitialisationArgs);
+        
+        return "TODO";
+    }
 }
