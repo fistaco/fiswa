464527299d458ea45b96f47b2cfc231a26dd0d70
fistaco <fiske_schijlen@hotmail.com>
2023-04-12 20:31:23 +0200

    Add skeleton for a GA controller that'll handle GA-related requests.

---
+++ src/main/java/com/example/demo/GeneticAlgorithmController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.GeneticAlgorithm;
+import com.example.entity.CrossoverType;
+import com.example.entity.GenomeType;
+
+
+/**
+ * Runs a GA and serves its results at the request of a user.
+ */
+@RestController
+public class GeneticAlgorithmController {
+    
+}
